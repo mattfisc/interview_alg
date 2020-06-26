4c07dc8ed1903e469c575ca8ecb99c68f2af49ba
Matthew Fischer <mattfisc@gmail.com>
2020-06-26 11:47:44 -0700

    partition complete

---
+++ partition_list.py
@@ -0,0 +1,15 @@
+def partition_list(nums, k):
+    nums.sort()
+    for i in range(len(nums)):
+        if nums[i] < k:
+            continue
+        elif nums[i] == 3:
+
+
+
+arr = [2, 2, 2, 5,2, 2, 2, 2, 5]
+k=3
+
+
+print(partition_list(arr, k))
+# [2, 2, 2, 2, 2, 2, 2, 2, 5]
